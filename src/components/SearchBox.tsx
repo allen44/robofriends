13efb7d2680956169e5b9b8d59c615165e730a71
allen44 <allen44@users.noreply.github.com>
2020-10-24 09:48:56 -1000

    initial commmit

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
+import * as React from 'react';
 
-const SearchBox = ({ searchfield, searchChange }) => {
+const SearchBox = ({ searchChange }) => {
   return (
     <div className='pa2'>
       <input
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
-        aria-label="Searchbox"
         placeholder='search robots'
         onChange={searchChange}
       />
     </div>
   );
-}
+};
 
 export default SearchBox;
