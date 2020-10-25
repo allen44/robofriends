386055468dc2430e11fad003bbd44a5df18ae572
allen44 <allen44@users.noreply.github.com>
2020-10-25 07:59:51 -1000

    add unit tests for MainPage

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -5,25 +5,28 @@ import SearchBox from './SearchBox';
 import Scroll from './Scroll';
 import ErrorBoundary from './ErrorBoundary';
 import './MainPage.css';
+import CounterButton from './CounterButton';
 
 class MainPage extends Component {
   componentDidMount() {
     this.props.onRequestRobots();
   }
-
-  render() {
-    const { robots, searchField, onSearchChange, isPending } = this.props;
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  filterRobots = () => {
+    return this.props.robots.filter(robot => {
+        return robot.name.toLowerCase().includes(this.props.searchField.toLowerCase());
     })
+  }
+  render() {
+    const { robots, onSearchChange, isPending } = this.props;
     return (
       <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={onSearchChange}/>
+        <CounterButton />
+        <SearchBox searchChange={onSearchChange}/> 
         <Scroll>
           { isPending ? <h1>Loading</h1> :
             <ErrorBoundary>
-              <CardList robots={filteredRobots} />
+              <CardList robots={this.filterRobots(robots)} />
             </ErrorBoundary>
           }
         </Scroll>
